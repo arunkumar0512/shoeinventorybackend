8e3576cbd972617fd8a8464fecd0702031d3a910
arunkumar0512 <140463247+arunkumar0512@users.noreply.github.com>
2024-01-05 17:43:20 +0530

    Update item.router.js

--- item.router.js
+++ item.router.js
@@ -1,20 +1,9 @@
-// import express from 'express'
-// import { deleteproductdetails, getproductdetails, productdetails } from '../Controller/item.controller.js';
 
-// const router=express.Router()
-
-// router.post('/product',productdetails)
-// router.get('/get/product',getproductdetails)
-// router.delete('/delete/product/:id',deleteproductdetails)
-
-
-
-// export default router;    
 
 
 import express from 'express'
-import { deleteproductdetails, getUserById, getproductdetails, loginform, productdetails, registerUser } from '../Controller/item.controller.js';
-import authMiddleware from '../Middleware/auth.middleware.js';
+import { deleteproductdetails, getUserById, getproductdetails, loginform, productdetails, registerUser } from './item.controller.js';
+import authMiddleware from './auth.middleware.js';
 
 const router=express.Router()
 
